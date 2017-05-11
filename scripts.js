18de521013e3aa0b49bb0c83e083bb9b6450cdd3
Ramon Jr. Yniguez <jryniguez@gmail.com>
2017-05-11 08:25:14 -0400

    updates html and js
    included multiple jQuery functions with conditions involving user interaction

--- scripts.js
+++ scripts.js
@@ -8,6 +8,47 @@ $(document).ready(function(){
             }
         } 
     };
+	var number = "";
+    var newnumber = "";
+    var operator = "";
+    var totaldiv = $("#total");
+    totaldiv.text("0");
+    $("#numbers a").not("#clear,#clearall").click(function(){
+		number += $(this).text();
+		totaldiv.text(number);
+		testNumLength(number);
+    });
+    $("#operators a").not("#equals").click(function(){
+		
+		operator = $(this).text();
+		newnumber = number;
+		number = "";
+		totaldiv.text("0");
 
-	
+    });
+    $("#clear,#clearall").click(function(){
+		number = "";
+		totaldiv.text("0");
+		if ($(this).attr("id") === "clearall") {
+			newnumber = "";
+		}
+    });
+    $("#equals").click(function(){
+		if (operator === "+"){
+			number = (parseInt(number, 10) + parseInt(newnumber,10)).toString(10);
+		} else if (operator === "-"){
+			number = (parseInt(newnumber, 10) - parseInt(number,10)).toString(10);
+		} else if (operator === "/"){
+			number = (parseInt(newnumber, 10) / parseInt(number,10)).toString(10);
+		} else if (operator === "*"){
+			number = (parseInt(newnumber, 10) * parseInt(number,10)).toString(10);
+		} 
+		else if (operator === "Sqrt"){
+			number = Math.sqrt(number).toString(10);
+		}
+		totaldiv.text(number);
+		testNumLength(number);
+		number = "";
+		newnumber = "";
+    });
 });
