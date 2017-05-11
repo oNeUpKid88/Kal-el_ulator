5699efddece429669a710cd08d16f30e6c779aae
Ramon Jr. Yniguez <jryniguez@gmail.com>
2017-05-10 21:13:59 -0400

    made significant improvements
    included cursor interactions with updates made toward css, updated Js src to accept="jQuery" host (provided by google beginning improvements toward Js file

--- scripts.js
+++ scripts.js
@@ -1,4 +1,13 @@
-window.onload = function () {
-    // your code here
-    var changeInner = document.getElementById('lets_go')[0].innerHTML = 'Lemme Go!'
-};
+$(document).ready(function(){
+	var testNumLength = function(number) {
+        if (number.length > 9) {
+            totaldiv.text(number.substr(number.length-9,9));
+            if (number.length > 15) {
+                number = "";
+                totaldiv.text("Err");
+            }
+        } 
+    };
+
+	
+});
