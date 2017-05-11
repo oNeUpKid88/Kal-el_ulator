dcd228842d64c30df387bb3d858d514ac6183ff6
Ramon Jr. Yniguez <jryniguez@gmail.com>
2017-05-11 11:56:27 -0400

    functionalityChanges
    I've made changes toward the calculators functionality. Calculation may now be performed while a bug has now become apparent and will now will be addressed.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	var testNumLength = function(number) {
+    var testNumLength = function(number) {
         if (number.length > 9) {
             totaldiv.text(number.substr(number.length-9,9));
             if (number.length > 15) {
@@ -8,47 +8,83 @@ $(document).ready(function(){
             }
         } 
     };
-	var number = "";
+    var number = "";
     var newnumber = "";
     var operator = "";
     var totaldiv = $("#total");
     totaldiv.text("0");
     $("#numbers a").not("#clear,#clearall").click(function(){
-		number += $(this).text();
-		totaldiv.text(number);
-		testNumLength(number);
+        number += $(this).text();
+        totaldiv.text(number);
+        testNumLength(number);
     });
-    $("#operators a").not("#equals").click(function(){
-		
-		operator = $(this).text();
-		newnumber = number;
-		number = "";
-		totaldiv.text("0");
-
+    $("#operators a,#side a").not("#equals, #decimal").click(function(){
+        if ($(this).text() === "sqrt"){
+            operator = "sqrt";
+            $("#equals").click();
+            return;
+        }
+        operator = $(this).text();
+        newnumber = number;
+        number = "";
+        totaldiv.text("0");
     });
     $("#clear,#clearall").click(function(){
-		number = "";
-		totaldiv.text("0");
-		if ($(this).attr("id") === "clearall") {
-			newnumber = "";
-		}
+        number = "";
+        totaldiv.text("0");
+        if ($(this).attr("id") === "clearall") {
+            newnumber = "";
+        }
     });
+    $("#decimal").click(function(){
+        var numOfDecs = 0;
+        for (i=0;i<number.length;i++){
+            if (number[i] === "."){
+            numOfDecs++;
+            }
+        }
+        if (numOfDecs > 0) {
+            number = number;
+            number = round(number,9);
+            totaldiv.text(number);
+        } else {
+            number += ".";
+            number = round(number,9);
+            totaldiv.text(number);
+        }
+        testNumLength(number);
+    });
+    //Add your last .click() here!
     $("#equals").click(function(){
-		if (operator === "+"){
-			number = (parseInt(number, 10) + parseInt(newnumber,10)).toString(10);
-		} else if (operator === "-"){
-			number = (parseInt(newnumber, 10) - parseInt(number,10)).toString(10);
-		} else if (operator === "/"){
-			number = (parseInt(newnumber, 10) / parseInt(number,10)).toString(10);
-		} else if (operator === "*"){
-			number = (parseInt(newnumber, 10) * parseInt(number,10)).toString(10);
-		} 
-		else if (operator === "Sqrt"){
-			number = Math.sqrt(number).toString(10);
-		}
-		totaldiv.text(number);
-		testNumLength(number);
-		number = "";
-		newnumber = "";
+        if (operator === "+"){
+            number = (parseFloat(number, 10) + parseFloat(newnumber,10)).toString(10);
+        } else if (operator === "-"){
+            number = (parseFloat(newnumber, 10) - parseFloat(number,10)).toString(10);
+        } else if (operator === "÷"){
+            number = (parseFloat(newnumber, 10) / parseFloat(number,10)).toString(10);
+        } else if (operator === "×"){
+            number = (parseFloat(newnumber, 10) * parseFloat(number,10)).toString(10);
+        } else if (operator === "sqrt"){
+            number = Math.sqrt(parseFloat(number,10).toString(10))
+        }else if  (operator === "^"){
+            number = Math.pow(number,newnumber)
+        }
+        totaldiv.text(number);
+        testNumLength(number);
+        number = "";
+        newnumber = "";
     });
+    var valueContainer = function(value){
+        num = value
+        nwnum = num
+        num = ""
+        counter = 0
+        if(counter == 0){
+            console.log("max number in storage has been reached")
+        }
+        $("#storedValues").Click(function(){
+
+        });   
+}
+
 });
